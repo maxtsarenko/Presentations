d0d5e93a33abc394eca696bade4fb2d821fc7af7
maksym tsarenko <max.tsarenko@gmail.com>
2016-07-11 22:50:51 +0300

    add Demo01.WellKnownLeaks project

---
+++ Demo01.WellKnownLeaks/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo01.WellKnownLeaks
+{
+    internal class Program
+    {
+        private const string MakeASnapshotHere = "Make a snapshot here then press Enter to continue";
+
+        static void Main(string[] args)
+        {
+            // https://gist.github.com/maxtsarenko/ddd94eb948c00f41dba4b1420dfffdaa
+
+            Console.WriteLine(MakeASnapshotHere);
+            Console.ReadLine();
+
+            Console.WriteLine("Creating citizen...");
+
+            var citizen = new Citizen();
+
+            Console.Out.WriteLine("Converting him to zombie...");
+
+            ConvertToZombie(citizen);
+
+            Console.Out.WriteLine(MakeASnapshotHere + " and see if citizen is infected");
+            Console.ReadLine();
+
+            Console.WriteLine($"Citizen is infected: {citizen.IsInfected}");
+
+            Console.ReadKey();
+        }
+
+        private static void ConvertToZombie(Citizen citizen)
+        {
+            var zombie = new Zombie(bones: new Kg(40), flesh: new Kg(60));
+
+            citizen.Yelled += zombie.BiteAndInfect;
+
+            citizen.Freighten();
+        }
+    }
+
+
+    internal class Citizen
+    {
+        internal event EventHandler Yelled;
+
+        internal bool IsInfected { get; set; }
+
+        internal void Freighten()
+        {
+            Yelled?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    internal class Zombie
+    {
+        internal Zombie(Kg bones, Kg flesh)
+        {
+            Bones = bones;
+            Flesh = flesh;
+        }
+
+        internal Kg Flesh { get; }
+
+        internal Kg Bones { get; }
+
+        internal void BiteAndInfect(object sender, EventArgs args)
+        {
+            var citizen = sender as Citizen;
+            if (citizen != null)
+            {
+                citizen.IsInfected = true;
+
+                Console.Out.WriteLine($"citizen {citizen} was infected by zombie with {Flesh} of flesh and {Bones} of bones");
+            }
+        }
+    }
+
+    internal class Kg
+    {
+        private readonly int[] _gram;
+
+        public Kg(int kg)
+        {
+            _gram = Enumerable.Repeat(1, kg * 1000).ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"{_gram.Length / 1000} kg";
+        }
+    }
+}
